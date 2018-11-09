68bbb281a628f62b3ecb0c536a268159dc3facde
Reisse <ruinonfire@gmail.com>
2018-11-09 13:28:18 +0300

    Add a few comments to mod class

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -1,5 +1,10 @@
 package example;
 
 public class Example {
-    
+    // in this class
+    // implement Rift listener interfaces such as BlockAdder, ItemAdder
+
+    // see open sourced mods for reference
+
+    // full names of listener classes must be placed in riftmod.json file
 }
